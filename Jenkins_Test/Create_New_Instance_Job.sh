1bc4e07615a21c156d4941620d98ce8c82dd6624
Cloud User <centos@ip-172-31-1-90.ec2.internal>
2021-10-19 13:18:28 +0000

    Updated the script to remove the unwanted IP variable

--- Jenkins_Test/Create_New_Instance_Job.sh
+++ Jenkins_Test/Create_New_Instance_Job.sh
@@ -5,8 +5,8 @@
 #Function to Create a New AWS Spot Instance
 Create_New_Spot_Instance() {
 	echo "Creating the Instance : $COMPONENT"
-	IP=$(aws ec2 run-instances --launch-template LaunchTemplateId=${LaunchTemplate_ID},Version=$Default \
-        --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$COMPONENT}]" "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]")
+	aws ec2 run-instances --launch-template LaunchTemplateId=${LaunchTemplate_ID},Version=$Default \
+        --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$COMPONENT}]" "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]"
         echo $IP
 }
 
