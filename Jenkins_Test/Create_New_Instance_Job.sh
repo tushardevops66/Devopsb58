d74d69a64640f9dec17c1cfbc5cba3492e4fa232
Cloud User <centos@ip-172-31-1-90.ec2.internal>
2021-10-30 15:45:21 +0000

    Included a new fle for session details

--- Jenkins_Test/Create_New_Instance_Job.sh
+++ Jenkins_Test/Create_New_Instance_Job.sh
@@ -5,9 +5,8 @@
 #Function to Create a New AWS Spot Instance
 Create_New_Spot_Instance() {
 	echo "Creating the Instance : $COMPONENT"
-	aws ec2 run-instances --launch-template LaunchTemplateId=${LaunchTemplate_ID},Version=$Default \
+	aws ec2 run-instances --launch-template LaunchTemplateId=${LaunchTemplate_ID},Version=Default \
         --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=$COMPONENT}]" "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]"
-        echo $IP
 }
 
 if [ -z "$COMPONENT" ]; then
